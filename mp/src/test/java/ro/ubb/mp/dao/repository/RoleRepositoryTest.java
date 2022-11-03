9420c8d677ab0dc4e7ffd3aba98a250250a9e266
Lavinia Tinca <lavinia_tinca@yahoo.com>
2022-11-03 19:37:39 +0200

    feature(CP-59): Added Tests for Save and Delete Role

---
+++ mp/src/test/java/ro/ubb/mp/dao/repository/RoleRepositoryTest.java
@@ -0,0 +1,38 @@
+package ro.ubb.mp.dao.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import ro.ubb.mp.dao.model.Role;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class RoleRepositoryTest {
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Test
+    public void saveRole(){
+        Role r1 = Role.builder()
+                .name("student")
+                .build();
+        roleRepository.save(r1);
+
+        Role r2 = Role.builder()
+                .name("mentor")
+                .build();
+        roleRepository.save(r1);
+        Role r3 = Role.builder()
+                .name("bothJPA")
+                .build();
+        roleRepository.save(r1);
+        roleRepository.save(r2);
+        roleRepository.save(r3);
+    }
+    @Test
+    public void deleteRole(){
+        roleRepository.deleteAll();
+    }
+}
