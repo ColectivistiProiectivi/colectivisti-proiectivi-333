9df78068e29e3b6f27d94980e03af77b95c7d98c
Lavinia Tinca <lavinia_tinca@yahoo.com>
2022-11-03 19:00:16 +0200

    feature(CP-59): Added Users and Role tables

--- mp/src/main/java/ro/ubb/mp/dao/model/User.java
+++ mp/src/main/java/ro/ubb/mp/dao/model/User.java
@@ -1,19 +1,66 @@
 package ro.ubb.mp.dao.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @Entity(name = "users")
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "user_email_unique", columnNames = "email")
+        }
+)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
-
-    @GeneratedValue
     @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
     private Long id;
 
-    private String firstName;
-    private String lastName;
+    @Column(
+            name = "fullName",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String fullName;
+
+    @Column(
+            name = "email",
+            nullable = false
+    )
+    private String email;
+
+    @Column(
+            name = "password",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String password;
+
+    @Column(
+            name = "profile_pic",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String profile_pic;
+
+    private String interestAreas;
 }
