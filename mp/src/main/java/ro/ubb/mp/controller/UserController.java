a91cdfa1bd83658248d2ac32f381b516d8eb75af
Lavinia Tinca <lavinia_tinca@yahoo.com>
2022-11-09 14:23:39 +0200

    feature(CP-60): register endpoint

--- mp/src/main/java/ro/ubb/mp/controller/UserController.java
+++ mp/src/main/java/ro/ubb/mp/controller/UserController.java
@@ -3,24 +3,30 @@ package ro.ubb.mp.controller;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ro.ubb.mp.controller.dto.UserRequestDTO;
 import ro.ubb.mp.dao.model.User;
 import ro.ubb.mp.service.user.UserService;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+//@RequestMapping()
 @RequiredArgsConstructor
 @Getter
 public class UserController {
     private final UserService userService;
 
-    @GetMapping
+    @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok().body(userService.getAll());
+    }
+
+    @PostMapping("/user/register")
+    public ResponseEntity<User> register(@RequestBody UserRequestDTO user){
+        URI uri =URI.create((ServletUriComponentsBuilder.fromCurrentContextPath().path("/user/register").toUriString()));
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
     }
 }
