43c57bf5c0d5fc14c8d6c2ae6bc3cefe98929eae
Andy <97806404+silverbabyyyy@users.noreply.github.com>
2022-11-09 21:59:41 +0300

    feature(CP-64): Create Login page/form (#4)

--- fe/src/features/application/App.tsx
+++ fe/src/features/application/App.tsx
@@ -4,19 +4,28 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { theme } from './theme'
 
 import { LoadingScreen } from './LoadingScreen'
+import LoginPage from '../login/LoginPage'
 
 const RegisterPage = React.lazy(() => import('../registration/RegisterPage'))
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Work in progress...</div>,
+    element: <div>Dashboard in progress...</div>,
   },
   {
     path: '/register',
     element: (
       <LoadingScreen>
         <RegisterPage />
+      </LoadingScreen>
+    ),
+  },
+  {
+    path: '/login',
+    element: (
+      <LoadingScreen>
+        <LoginPage />
       </LoadingScreen>
     ),
   },
