5bc7905a89591a3977ec51764cb0006b4b7accd3
Lavinia Tinca <lavinia_tinca@yahoo.com>
2022-11-09 21:06:04 +0200

    feature(CP-60): functional register

--- mp/src/main/java/ro/ubb/mp/controller/AuthController.java
+++ mp/src/main/java/ro/ubb/mp/controller/AuthController.java
@@ -4,21 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ro.ubb.mp.controller.dto.UserRequestDTO;
 import ro.ubb.mp.dao.model.User;
-import ro.ubb.mp.dto.UserDTO;
 import ro.ubb.mp.service.user.UserService;
 
 import java.net.URI;
 
+@RestController
 public class AuthController {
-
+    @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody UserRequestDTO user){
-        URI uri =URI.create((ServletUriComponentsBuilder.fromCurrentContextPath().path("/register").toUriString()));
+    public ResponseEntity<User> register(@RequestBody UserRequestDTO user) {
+        URI uri = URI.create((ServletUriComponentsBuilder.fromCurrentContextPath().path("/register").toUriString()));
         return ResponseEntity.created(uri).body(userService.saveUser(user));
     }
 }
