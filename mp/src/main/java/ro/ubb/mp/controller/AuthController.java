6db9004b937d575f2d7099923cad73bdff031b89
Lavinia Tinca <lavinia_tinca@yahoo.com>
2022-11-09 18:43:02 +0200

    feature(CP-60): starting Auth Controller2

--- mp/src/main/java/ro/ubb/mp/controller/AuthController.java
+++ mp/src/main/java/ro/ubb/mp/controller/AuthController.java
@@ -1,13 +1,18 @@
 package ro.ubb.mp.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ro.ubb.mp.dao.model.User;
 import ro.ubb.mp.dao.repository.UserRepository;
 import ro.ubb.mp.dto.UserDTO;
 import ro.ubb.mp.service.user.UserService;
+
+import java.net.URI;
 
 public class AuthController {
     private UserService userService;
@@ -29,5 +34,11 @@ public class AuthController {
         userService.saveUser(userDTO);
 
         return "register_success";
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody UserDTO user){
+        URI uri =URI.create((ServletUriComponentsBuilder.fromCurrentContextPath().path("/user/register").toUriString()));
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
     }
 }
