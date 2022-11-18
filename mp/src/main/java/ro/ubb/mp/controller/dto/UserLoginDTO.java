66e66a7725fd1c0bf6ac479c63766f3057c02e90
giuliastan7 <giulia.stan10@yahoo.com>
2022-11-18 12:27:47 +0200

    -register validations(for empty fields)+password has minimum 7 caracters -added a validation dependency

--- mp/src/main/java/ro/ubb/mp/controller/dto/UserLoginDTO.java
+++ mp/src/main/java/ro/ubb/mp/controller/dto/UserLoginDTO.java
@@ -3,11 +3,13 @@ package ro.ubb.mp.controller.dto;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 public class UserLoginDTO {
     @NotBlank
     private String email;
     @NotBlank
+    @Size(min=7)
     private String password;
 }
