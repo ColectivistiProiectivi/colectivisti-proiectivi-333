95b90dcd85ee71ef7cbb19a9db97365400a3df61
Cecilia Andreea Florina Trif <79456848+CeciliaTrif@users.noreply.github.com>
2023-01-18 19:59:17 +0200

    Added endpoint for all users that have an appointment with a certain mentor.

--- mp/src/main/java/ro/ubb/mp/dao/repository/AppointmentRepository.java
+++ mp/src/main/java/ro/ubb/mp/dao/repository/AppointmentRepository.java
@@ -1,12 +1,10 @@
 package ro.ubb.mp.dao.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ro.ubb.mp.dao.model.Appointment;
 import ro.ubb.mp.dao.model.User;
-
 
 import java.util.List;
 
--- mp/src/main/resources/static/data.sql
+++ mp/src/main/resources/static/data.sql
@@ -1,7 +1,10 @@
 INSERT INTO users(id, birthdate, description, full_name, password, role, username)
 VALUES
     (-1, '1992-12-12', '', 'Ted Tod', '$2a$10$arEsCEapqLiMznmtaez7rOyUT.mRcNIZg4r5kYQ2Yerqicf5cIVfe', 'MENTOR', 'tedtod@test.com'),
-    (-2, '1993-02-14', '', 'Freddy Jameson', '$2a$10$arEsCEapqLiMznmtaez7rOyUT.mRcNIZg4r5kYQ2Yerqicf5cIVfe', 'STUDENT', 'fjameson@test.com');
+    (-2, '1993-02-14', '', 'Freddy Jameson', '$2a$10$arEsCEapqLiMznmtaez7rOyUT.mRcNIZg4r5kYQ2Yerqicf5cIVfe', 'STUDENT', 'fjameson@test.com'),
+    (-3, '1993-02-14', '', 'Emma Williams', '$2a$10$arEsCEapqLiMznmtaez7rOyUT.mRcNIZg4r5kYQ2Yerqicf5cIVfe', 'STUDENT', 'ewilliams@test.com'),
+    (-4, '1993-02-14', '', 'Michael Brown', '$2a$10$arEsCEapqLiMznmtaez7rOyUT.mRcNIZg4r5kYQ2Yerqicf5cIVfe', 'STUDENT', 'mbrown@test.com'),
+    (-5, '1993-02-14', '', 'Olivia Jones', '$2a$10$arEsCEapqLiMznmtaez7rOyUT.mRcNIZg4r5kYQ2Yerqicf5cIVfe', 'STUDENT', 'ojones@test.com');
 
 --  All the passwords are "test123"
 --  Move this file to /src/main/resources if you want to use it to initialize your database
@@ -18,3 +21,8 @@ VALUES
     (-2, 'Matematica'),
     (-1, 'Chimie');
 
+INSERT INTO appointments(id, date, location_details, mentor_id, student_id)
+VALUES
+    (-1, '2023-01-15 19:00:00.000000', 'zoom meet', -1, -2),
+    (-2, '2023-01-15 19:00:00.000000', 'zoom meet', -1, -3),
+    (-3, '2023-01-15 19:00:00.000000', 'zoom meet', -1, -4);
