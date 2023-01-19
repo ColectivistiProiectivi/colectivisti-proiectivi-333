19c03f426cdaf83fd26355631162a8996acf089a
Mihai Gabriel <17096004+mihai-gabriel@users.noreply.github.com>
2023-01-20 00:08:28 +0200

    refactor: Use Different DTO to get user IDs and exclude sensitive information like the password

--- mp/src/main/java/ro/ubb/mp/controller/dto/mapper/AssignmentMapper.java
+++ mp/src/main/java/ro/ubb/mp/controller/dto/mapper/AssignmentMapper.java
@@ -1,10 +1,13 @@
 package ro.ubb.mp.controller.dto.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import ro.ubb.mp.controller.dto.response.assignment.AssignmentResponseDTO;
 import ro.ubb.mp.dao.model.Assignment;
 
 @Mapper(componentModel = "spring")
 public interface AssignmentMapper {
+    @Mapping(target = "author.email", source = "author.username")
     AssignmentResponseDTO toDTO(Assignment assignment);
 }
