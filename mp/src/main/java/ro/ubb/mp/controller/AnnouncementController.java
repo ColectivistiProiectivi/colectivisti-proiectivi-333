58cb232e001883a5b3915f33bfa5f4652a73b15d
carmen <titicarmen21@gmail.com>
2023-01-20 22:42:46 +0200

    refactor post announcement

--- mp/src/main/java/ro/ubb/mp/controller/AnnouncementController.java
+++ mp/src/main/java/ro/ubb/mp/controller/AnnouncementController.java
@@ -55,10 +55,10 @@ public class AnnouncementController {
     }
 
     @PostMapping()
-    public ResponseEntity<Announcement> addAnnouncement(@RequestBody AnnouncementRequestDTO announcement) {
+    public ResponseEntity<AnnouncementResponseDTO> addAnnouncement(@RequestBody AnnouncementRequestDTO announcement) {
         URI uri = URI.create((ServletUriComponentsBuilder.fromCurrentContextPath().path("/addAnnouncement").toUriString()));
 
-        return ResponseEntity.created(uri).body(getAnnouncementService().saveAnnouncement(announcement));
+        return ResponseEntity.created(uri).body(getAnnouncementMapper().toDTO(getAnnouncementService().saveAnnouncement(announcement)));
     }
 
     @DeleteMapping("/{id}")
