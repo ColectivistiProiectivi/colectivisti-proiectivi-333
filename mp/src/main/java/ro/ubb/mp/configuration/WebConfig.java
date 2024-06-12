765fbb9b2c2b92aafccadfc70478f512b070f777
Zimbru Florin <florinz@cicadatech.eu>
2024-06-12 20:30:09 +0300

    FZ/commit

--- mp/src/main/java/ro/ubb/mp/configuration/WebConfig.java
+++ mp/src/main/java/ro/ubb/mp/configuration/WebConfig.java
@@ -14,8 +14,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://your-frontend-domain.com", "http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
